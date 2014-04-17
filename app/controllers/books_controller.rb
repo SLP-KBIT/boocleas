2520c07d583e9a89e25e69f89d327117c6cc7f81
iguto <clc.iguto@gmail.com>
2014-04-18 04:20:32 +0900

    書籍の登録を実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ require 'ndl'
 
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
   def new
     @book = Book.new
@@ -11,5 +12,19 @@ class BooksController < ApplicationController
     render text: "本が見つかりませんでした。" and return unless params_from_api
     @api = params_from_api
     @book = Book.new(params_from_api)
+  end
+  def create
+    @book = Book.new(book_params)
+    @book = nil unless @book.save
+    unless @book
+      flash.now[:error] = "書籍の追加に失敗しました。"
+      render :new and return
+    end
+    flash[:success] = "書籍を登録しました。"
+    redirect_to new_book_path
+  end
+  private
+  def book_params
+    params.require(:book).permit(%i(title author isbn publisher published_at keyword))
   end
 end
