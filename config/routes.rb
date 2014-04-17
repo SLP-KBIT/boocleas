2d39beba85fa935f2bd5306437bc9579d8107e8f
iguto <clc.iguto@gmail.com>
2014-04-17 23:48:32 +0900

    レイアウトを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 BookAdmin::Application.routes.draw do
-  resources :book
-  resources :biblio
-  resources :shelf
+  get "books/index"
+  resources :books
+  resources :biblios
+  resources :shelves
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
