e51887298314b9d7e616bf652e14bd8ce12577f5
Club09 s09t214 OOKAWA Masahiro <clc.iguto@gmail.com>
2014-04-25 23:52:14 +0900

    annotate

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,13 +5,13 @@
 #  id           :integer          not null, primary key
 #  isbn         :string(255)
 #  author       :text
-#  published_at :integer
 #  publisher    :string(255)
 #  genre        :text
 #  keyword      :text
 #  created_at   :datetime
 #  updated_at   :datetime
 #  title        :text
+#  published_at :datetime
 #
 
 class Book < ActiveRecord::Base
