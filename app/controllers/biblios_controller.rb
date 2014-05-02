ba4b35f4cf7066c6ef402591c55066e72e0baa78
Club09 s09t214 OOKAWA Masahiro <clc.iguto@gmail.com>
2014-05-02 17:27:31 +0900

    close #16 貸出履歴に蔵書へのリンクを追加

--- app/controllers/biblios_controller.rb
+++ app/controllers/biblios_controller.rb
@@ -1,12 +1,20 @@
 class BibliosController < ApplicationController
   before_action :authenticate_user!
+  before_action :get_biblio
   def index
     form_params = params[:biblio_form] || nil
     @biblio_form = BiblioForm.new form_params
     @biblios = @biblio_form.search
   end
   def new
-    @book = Book.where(id: params[:book_id]).first
     @biblio = @book.biblios.build
+  end
+  def show
+  end
+
+  private
+
+  def get_biblio
+    @biblio = Biblio.where(id: params[:id]).first if params[:id]
   end
 end
