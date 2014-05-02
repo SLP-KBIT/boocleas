08a670a6ca8d82a751b7369c2fa1d2ee556d65ee
Club09 s09t214 OOKAWA Masahiro <clc.iguto@gmail.com>
2014-05-02 17:10:02 +0900

    close #10 貸出禁止ユーザへの貸出を拒否する

--- app/controllers/lent_histories_controller.rb
+++ app/controllers/lent_histories_controller.rb
@@ -13,13 +13,17 @@ class LentHistoriesController < ApplicationController
     @history = biblio.lent_histories.build
   end
   def create
+    if !current_user.is_lendable && params[:commit] == "貸出"
+      flash[:error] = "あなたは貸出禁止ユーザのため、貸し出しできません。"
+      redirect_to my_path and return
+    end
     biblio = Biblio.where(id: params[:lent_history][:biblio_id]).first
     render '/public/404.html' and return unless biblio.changeable_to?(state_from_commit)
     @history = biblio.lent_histories.build(user_id: current_user.id, state: state_from_commit)
     render '/public/404.html' and return unless @history.save
     flash[:success] = "#{@history.biblio.book.title} を貸し出しました。" if params[:commit] == "貸出"
     flash[:success] = "#{@history.biblio.book.title} を返却しました。" if params[:commit] == "返却"
-    redirect_to my_path
+    redirect_to my_path and return
   end
 
   private
