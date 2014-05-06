74911063abe8e057c5600628bb45b347070d2e7f
Club09 s09t214 OOKAWA Masahiro <clc.iguto@gmail.com>
2014-05-07 01:59:12 +0900

    close #20 最近借りた蔵書を、最近借りた順に表示するように変更

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -3,6 +3,6 @@ class MyController < ApplicationController
   def index
     @biblios = LentHistory.lent_to(current_user).map(&:biblio)
     @biblo_form = BiblioForm.new params[:biblio_form]
-    @histories = LentHistory.where(user_id: current_user.id).group(:biblio_id)
+    @histories = LentHistory.where(user_id: current_user.id).group(:biblio_id).order("created_at DESC")
   end
 end
