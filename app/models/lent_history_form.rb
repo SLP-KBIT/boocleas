22c585cb783aaa3168c40345e375438606c7ff2f
Club09 s09t214 OOKAWA Masahiro <clc.iguto@gmail.com>
2014-05-08 01:45:27 +0900

    履歴のタイトルによる検索を実装

---
+++ app/models/lent_history_form.rb
@@ -0,0 +1,15 @@
+class LentHistoryForm
+  include ActiveModel::Model
+
+  attr_accessor :title
+
+  def initialize(attributes={})
+    super
+  end
+
+  def search
+    histories = LentHistory.all
+    histories = histories.select { |hist| hist.biblio.book.title.include? @title } if @title
+    histories
+  end
+end
